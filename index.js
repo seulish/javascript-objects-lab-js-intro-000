5a25101d844c5eae790d09c4b3efc3ac4cf6789e
Leah Lee <leah@iterable.com>
2021-02-05 02:03:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   Object.assign({}, obj, { [key]: value })
   return obj
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
+}
